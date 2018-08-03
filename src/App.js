b3fbde8017afae787513f8cbed3218eac588440c
Brent Danley <bdanley@3blmedia.com>
2018-08-03 18:44:12 -0400

    TP-3 Makes Header component functional. Adds CSS file to same.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 class App extends Component {
