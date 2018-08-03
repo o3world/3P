89323e9be086c919942561f6e90c892f4e0cf706
Brent Danley <bdanley@3blmedia.com>
2018-08-03 13:43:33 -0400

    TP-4 Creates Footer component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from "./Header";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <Header/>
           <h1>Triple Pundit</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Graecis hoc modicum est: Leonidas, Epaminondas, tres aliqui aut quattuor; Sed haec in pueris; Duo Reges: constructio interrete. Consequentia exquirere, quoad sit id, quod volumus, effectum. Sed plane dicit quod intellegit. Duarum enim vitarum nobis erunt instituta capienda. Nos paucis ad haec additis finem faciamus aliquando;</p>
+          <Footer/>
       </div>
     );
   }
