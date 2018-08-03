a99ac89b6046d356681689e2e2f22096cec49e17
Brent Danley <bdanley@3blmedia.com>
2018-08-03 13:07:47 -0400

    TP-3 Creates Header component and adds it to the App.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header/>
           <h1>Triple Pundit</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Graecis hoc modicum est: Leonidas, Epaminondas, tres aliqui aut quattuor; Sed haec in pueris; Duo Reges: constructio interrete. Consequentia exquirere, quoad sit id, quod volumus, effectum. Sed plane dicit quod intellegit. Duarum enim vitarum nobis erunt instituta capienda. Nos paucis ad haec additis finem faciamus aliquando;</p>
       </div>
