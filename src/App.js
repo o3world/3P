d33b02f270f2925b7f471d86d56ffb679d339970
Brent Danley <bdanley@3blmedia.com>
2018-08-03 18:33:38 -0400

    TP-4 Makes Footer component functional. Adds CSS to same.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-          <h1>Triple Pundit</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Graecis hoc modicum est: Leonidas, Epaminondas, tres aliqui aut quattuor; Sed haec in pueris; Duo Reges: constructio interrete. Consequentia exquirere, quoad sit id, quod volumus, effectum. Sed plane dicit quod intellegit. Duarum enim vitarum nobis erunt instituta capienda. Nos paucis ad haec additis finem faciamus aliquando;</p>
+        <Header site_title="Triple Pundit"/>
+        <p>Content</p>
           <Footer/>
       </div>
     );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = (props) => {
+    return (
+        <footer>This is the Footer.</footer>
+    );
+};
+
+export default Footer;
