61c218542eccfbc8ed6bb531cfff841fd22cc0c3
Brent Danley <bdanley@3blmedia.com>
2018-08-06 16:05:11 -0400

    TP-37 Install React Router. Sets up basic routing.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header/Header";
@@ -8,18 +13,42 @@ import Footer from "./components/Footer/Footer";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Header site_title="Triple Pundit"/>
-          <Home/>
+        <Router>
+        <div className="App">
+        <ul>
+        <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/topics">Topics</Link></li>
+      </ul>
+
+      <hr/>
+      <Header site_title="Triple Pundit"/>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/topics" component={Topics}/>
           <Footer/>
           <img src={logo} />
-      </div>
+  </div>
+  </Router>
     );
   }
 }
 
 const Home = () => (
     <Editors/>
+);
+
+const About = () => (
+    <div>
+      <h2>About</h2>
+    </div>
+);
+
+const Topics = () => (
+    <div>
+      <h3>Topics</h3>
+    </div>
 );
 
 export default App;
