466a0710cec3bf39ee12087bc7f4f7db35b537f0
Brent Danley <bdanley@3blmedia.com>
2018-08-06 15:09:11 -0400

    TP-17 Editors component.

--- src/components/Editors/Editors.js
+++ src/components/Editors/Editors.js
@@ -2,11 +2,12 @@ import React from 'react';
 // import './Editors.css';
 import Editor from './Editor';
 
-const data = require("../../data/editors.json");
+const data = require("./editors.json");
 
 const Editors = () => (
     <div className="editors__container">
-        <h3>Meet Our Editors</h3>
+        <h3>Editorial Team</h3>
+        <h4>Critical Writers & Purpose Driven News Tellers</h4>
         {data.map(editor =>
             <Editor {...editor}/>
         )}
