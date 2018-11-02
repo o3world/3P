611ff79f48228dd5d00a333959b08cad2d6e61db
Brent Danley <bdanley@3blmedia.com>
2018-11-02 11:50:12 -0400

    Adds alt tag to image element.

--- src/components/Home/HomeEditor.js
+++ src/components/Home/HomeEditor.js
@@ -4,7 +4,7 @@ import './HomeEditor.css';
 const HomeEditor = (props) => {
     return (
         <div className={'home-editor__wrapper'}>
-            <img className={'home-editor__headshot'} src={props.icon} />
+            <img className={'home-editor__headshot'} src={props.icon} alt={'editor headshot'} />
             <div className={'home-editor__meta-wrapper'}>
                 <h3 className="home-editor__name">{props.displayName}</h3>
                 <p className={'home-editor__title'}>{props.title}</p>
