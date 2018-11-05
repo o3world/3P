554a20ecac9c93d217fd6796e1684532f953630f
Brent Danley <bdanley@3blmedia.com>
2018-11-05 10:44:10 -0500

    Replaces SVG logo with PNG.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import './Header.css';
 
+import logo__large from '../../assets/images/3P_Logo_Homepage_Large.png';
+
 const Header = (props) =>
     <header className="header">
 
-      <svg width="269px" height="142px" viewBox="0 0 269 142" version="1.1">
-        <g id="Templates" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-          <g id="Homepage-With-Podcast" transform="translate(-129.000000, -82.000000)">
-            <g id="Group-7">
-              <g id="branding/logo-light-copy-7" transform="translate(129.000000, 75.000000)" fill="#000000" fontFamily="Montserrat-Black, Montserrat" fontSize="53" fontWeight="700" letterSpacing="13.17" line-spacing="50.96">
-                <text id="TRIPLE-PUNDIT">
-                  <tspan x="0" y="51">TRIPLE</tspan>
-                  <tspan x="328.279" y="64.6581488" fontSize="58.4064" letterSpacing="9.57216049"></tspan>
-                  <tspan x="37.8614" y="105.790576" fontFamily="Unna-Regular, Unna" fontSize="62.4" fontWeight="normal" letterSpacing="11.96">PUNDIT</tspan>
-                </text>
-              </g>
-              <g id="Group-5" transform="translate(130.000000, 199.000000)" line-spacing="16">
-                <text id="The-Business-of-Doin" fontFamily="Montserrat-Bold, Montserrat" fontSize="11.5" fontWeight="bold" letterSpacing="1.15" fill="#006BFF">
-                  <tspan x="18" y="11">THE BUSINESS OF DOING BETTER</tspan>
-                </text>
-                <text id="{" fontFamily="Montserrat-ExtraLight, Montserrat" fontSize="36" fontWeight="300" letterSpacing="1.3" fill="#000000">
-                  <tspan x="0" y="37">&#123;</tspan>
-                    </text>
-                    <text id="}" fontFamily="Montserrat-ExtraLight, Montserrat" fontSize="36" fontWeight="300" letterSpacing="1.3" fill="#000000">
-                    <tspan x="259" y="37">&#125;</tspan>
-                    </text>
-                    </g>
-                    </g>
-                    </g>
-                    </g>
-                    </svg>
+      <img src={logo__large} alt={'3bl logo'} />
     </header>
 
 export default Header;
