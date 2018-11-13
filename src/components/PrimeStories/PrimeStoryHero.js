409c7b5fd269312e9d281dac89706473b593e08c
Brent Danley <bdanley@3blmedia.com>
2018-11-13 17:37:23 -0500

    Formats date on Prime Story Hero card. Adds author name to same.

--- src/components/PrimeStories/PrimeStoryHero.js
+++ src/components/PrimeStories/PrimeStoryHero.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './PrimeStoryHero.css';
+import Moment from "react-moment";
 
 class PrimeStoryHero extends Component {
     render() {
         const storyStyle = {
             backgroundImage: `url(${this.props.fieldFeaturedImageWide.url})`
         };
+      const published_date = new Date(this.props.date * 1000);
         return (
             <div className={this.props.storyClass}>
                 <img src={this.props.fieldFeaturedImageWide.url} style={storyStyle} alt={'add alt'} className="prime-story-hero__image"/>
                 <div className="hero-meta">
                     <h3 className="prime-story__name">{this.props.title}</h3>
-                    <p className="author">Words by {this.props.author_name}</p>
-                    <time>{this.props.date}</time>
+                    <p className="author">Words by {this.props.author.first} {this.props.author.last}</p>
+                    <time><Moment format="MMM DD">{published_date}</Moment></time>
                 </div>
             </div>
         );
