138b043f0a4883b8cdabe8461556a064c111d65e
Brent Danley <bdanley@3blmedia.com>
2018-11-13 17:15:19 -0500

    Adds react-moment module. Formats date on Prime Story cards.

--- src/components/PrimeStories/PrimeStory.js
+++ src/components/PrimeStories/PrimeStory.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import './PrimeStory.css';
 
+import Moment from 'react-moment';
+
 class PrimeStory extends Component {
     render() {
         const storyStyle = {
             backgroundImage: `url(${this.props.fieldFeaturedImageTall.url})`
         };
+        const published_date = new Date(this.props.date * 1000);
         return (
             <div className={this.props.storyClass} style={storyStyle}>
                 <div className="story-card--content">
                     <h3 className="story-title">{this.props.title}</h3>
                     <p className="story-author">Words by {this.props.author.first} {this.props.author.last}</p>
                     <p className="story-category">{this.props.category.term.name}</p>
-                    <time className="story-publish-date">{this.props.date}</time>
+                    <time className="story-publish-date"><Moment format="MMM DD">{published_date}</Moment></time>
                 </div>
             </div>
         );
