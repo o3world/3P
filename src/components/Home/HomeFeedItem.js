4c7cb7378396674615eb7b512cbe36eeea7f14be
Brent Danley <bdanley@3blmedia.com>
2018-11-15 17:45:04 -0500

    Formats homepage feed item date using Moment.js.

--- src/components/Home/HomeFeedItem.js
+++ src/components/Home/HomeFeedItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './HomeFeedItem.css';
 
+import Moment from 'react-moment';
+
 const HomeFeedItem = (props) => {
     const {thumbnailLink, publishedDate, title} = props;
     return (
         <div className="feed-item" style={{backgroundImage: `url(${thumbnailLink})`}}>
             <div className="feed-item__meta-container">
-                <time className="feed-item__published-date">{publishedDate}</time>
+              <time className="feed-item__published-date"><Moment format="MMM DD">{publishedDate}</Moment></time>
                 <h3 className="feed-item__name">{title}</h3>
             </div>
         </div>
