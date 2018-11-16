eb0dac4d07a47dff607cafee58635194b27a137c
Brent Danley <bdanley@3blmedia.com>
2018-11-16 15:21:42 -0500

    Uses fetch to get The Feed data.

--- src/components/Home/HomeFeedItem.js
+++ src/components/Home/HomeFeedItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './HomeFeedItem.css';
 
+import Moment from 'react-moment';
+
 const HomeFeedItem = (props) => {
-    const {thumbnailLink, publishedDate, title} = props;
     return (
-        <div className="feed-item" style={{backgroundImage: `url(${thumbnailLink})`}}>
+        <div className="feed-item" style={{backgroundImage: `url(${props.thumbnailLink})`}}>
             <div className="feed-item__meta-container">
-                <time className="feed-item__published-date">{publishedDate}</time>
-                <h3 className="feed-item__name">{title}</h3>
+              <time className="feed-item__published-date"><Moment format="MMM DD">{props.publishedDate}</Moment></time>
+                <h3 className="feed-item__name">{props.title}</h3>
             </div>
         </div>
     )
