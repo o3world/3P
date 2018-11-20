ea0503c0cbaf31250410eea23e69a2b7520bcd1e
Brent Danley <bdanley@3blmedia.com>
2018-11-20 11:00:09 -0500

    TP-35 Styles primary header for full-width displays. Adds search and hamburger Font Awesome icons.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Header.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import logo__large from '../../assets/images/3P_Logo_Homepage_Large.png';
 
 const Header = (props) =>
     <header className="header">
 
-      <img src={logo__large} alt={'3bl logo'} />
+      <img className={'header-logo'} src={logo__large} alt={'3bl logo'} />
+      <FontAwesomeIcon className={'search-button'} icon={"search"} />
+      <FontAwesomeIcon className={'hamburger-button'} icon={"bars"} />
     </header>
 
 export default Header;
