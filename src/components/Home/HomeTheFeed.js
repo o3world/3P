aca2a4df6c29fa8bb502445b3db86f96be041c76
seacar <theseancarroll@gmail.com>
2018-11-30 13:51:51 -0600

    update to apollo

--- src/components/Home/HomeTheFeed.js
+++ src/components/Home/HomeTheFeed.js
@@ -1,38 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
 import HomeFeedItem from './HomeFeedItem';
 import './HomeTheFeed.css';
+import { Query } from "react-apollo";
+import gql from 'graphql-tag';
+import ApolloClient from "apollo-boost";
 
-class HomeTheFeed extends Component {
-  constructor(props) {
-    super(props);
+const customClient = new ApolloClient({
+  uri: "https://3blmedia.com/api/v1/feed"
+});
 
-    this.state = {
-      data: [],
-    };
-  }
-
-  componentDidMount() {
-    fetch('https://3blmedia.com/api/v1/feed')
-        .then(response => response.json())
-        .then(data => this.setState({ data: data.results }));
-  }
-
-  render() {
-
-        return (
-            <div className="the-feed__container">
-              <div className={'the-feed__title-wrapper'}>
-                <h3 className={'the-feed__title'}>The Feed</h3>
-                <p className={'the-feed__title--subtitle'}>Latest News from Leaders in Corporate Social Responsibility</p>
-              </div>
-              <div className="the-feed">
-                  {this.state.data.map(feeditem =>
-                      <HomeFeedItem {...feeditem} key={feeditem.id}/>
-                  )}
-              </div>
-            </div>
-        );
+const TheFeedQuery = gql`
+ query feedQuery {
+   results {
+     title
+     body
+     type
+     date
+     id
+     image
     }
   }
+`;
 
-export default HomeTheFeed;
+
+
+const TheFeed = () =>
+    <Query
+        query={TheFeedQuery}
+        client={customClient}
+        variables={{}}
+    >
+        {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
+
+            return (
+                <div className="the-feed__container">
+                  <div className={'the-feed__title-wrapper'}>
+                    <h3 className={'the-feed__title'}>The Feed</h3>
+                    <p className={'the-feed__title--subtitle'}>Latest News from Leaders in Corporate Social Responsibility</p>
+                  </div>
+                  <div className="the-feed">
+                      {this.state.data.map(feeditem =>
+                          <HomeFeedItem {...feeditem} key={feeditem.id}/>
+                      )}
+                  </div>
+                </div>
+            );
+        }}
+    </Query>
+
+export default TheFeed;
