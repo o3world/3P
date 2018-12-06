4668ad087dac132ff90d7b68d886fd031c25bf8f
Prasana Alawekar <prasana.alawekar@cuelogic.co.in>
2018-12-06 18:51:04 +0530

    completed api integration TP-52

--- src/components/FeedPage/Components/News.js
+++ src/components/FeedPage/Components/News.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
+import moment from 'moment';
+import "../CSS/News.css";
 
 const News = (props) => {
     let objFeed = props.data;
+    let description = ReactHtmlParser(objFeed.body)[0].props.children[0];
+    
     return(
         <div className="the-feed-main-box">
             <div className="feed-inner-box">
@@ -11,18 +16,19 @@ const News = (props) => {
                 <div className="feed-right-content">
                     <ul>
                         <li className="text-left">
-                            {objFeed.category}
+                            {/* {objFeed.category} */} ENERGY
                         </li>
                         <li className="text-right">
-                            {objFeed.date}
+                            { moment(objFeed.date).format("MMM D").toUpperCase()}
                         </li>
                     </ul>
                     <div className="feed-right-title-box">
                         <h4 className="feed-right-title">{objFeed.title}</h4>
-                        <p>{objFeed.description}</p>
+                        <p>{ description.slice(0,200) }</p>
                     </div>
                     <div className="feed-author-box">
-                        <p  className="author-name">{objFeed.author}</p>
+                        {/* <p  className="author-name">{objFeed.author}</p> */}
+                        <p  className="author-name">Proctor & Gamble</p>
                     </div>
                 </div>
             </div>
