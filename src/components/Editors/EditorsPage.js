e5eabb02ac0fa5ca50411633a7fdb43ad4b5842f
Brent Danley <bdanley@3blmedia.com>
2018-12-10 18:36:17 -0500

    Added curly braces to comment in JSX.

--- src/components/Editors/EditorsPage.js
+++ src/components/Editors/EditorsPage.js
@@ -10,7 +10,7 @@ const EditorsPage = () => (
     </div>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Istam voluptatem perpetuam quis potest praestare sapienti? Quo tandem modo? Et quidem, inquit, vehementer errat; Duo Reges: constructio interrete. Quamquam ab iis philosophiam et omnes ingenuas disciplinas habemus; Tu quidem reddes; Nec vero sum nescius esse utilitatem in historia, non modo voluptatem. Indicant pueri, in quibus ut in speculis natura cernitur. Expressa vero in iis aetatibus, quae iam confirmatae sunt. Ut placet, inquit, etsi enim illud erat aptius, aequum cuique concedere. His similes sunt omnes, qui virtuti student levantur vitiis, levantur erroribus, nisi forte censes Ti. Non quam nostram quidem, inquit Pomponius iocans;</p>
     <EditorCardsWrapper/>
-    /* @TODO Contributors component here */
+    { /* @TODO Contributors component here */ }
   </div>
 )
 
