abab906fba5db22649cca416f2cda264c37fb8e4
Brent Danley <bdanley@3blmedia.com>
2018-12-12 13:15:05 -0500

    Removes unused React imports to resolve build fails.

--- src/components/Stories/StoriesPage.js
+++ src/components/Stories/StoriesPage.js
@@ -1,9 +1,11 @@
-import React, {Component, Fragment} from 'react';
+import React from 'react';
 import QueryHoc from "../Common/Query";
 import * as Story from "./StoryQueries";
 import PrimeStoriesWrapper from '../PrimeStories/PrimeStoriesWrapper';
 import StoriesMasonryWrapper from "./StoriesMasonryWrapper";
+
 const variables = {};
+
 const StoriesPage = () => (
     <div>
         <PrimeStoriesWrapper/>
