2725eab515a0e799fb23db5bbd89db647c89fa06
Brent Danley <bdanley@3blmedia.com>
2018-12-11 20:03:18 -0500

    TP-73 Styles Sponsored Series All masonry wrapper.

--- src/components/SponsoredSeries/SponsoredSeriesAllMasonryWrapper.js
+++ src/components/SponsoredSeries/SponsoredSeriesAllMasonryWrapper.js
@@ -1,10 +1,14 @@
 import React from "react";
 import SponsoredSeriesMasonryTile from "./SponsoredSeriesMasonryTile";
 
+import styles from './SponsoredSeriesAllMasonryWrapper.module.scss';
+
 const SponsoredSeriesAllMasonryWrapper = (props) => {
     const series = props.data.taxonomyTermQuery.entities;
     return (
-        series.map((story, index) => <SponsoredSeriesMasonryTile data={story} key={index}/>)
+        <div className={styles.wrapper}>
+          {series.map((series, index) => <SponsoredSeriesMasonryTile data={series} key={index}/>)}
+        </div>
     );
 };
 
