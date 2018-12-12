0218fa378438c35f744dd6b70eaf34476b3e8d5b
Brent Danley <bdanley@3blmedia.com>
2018-12-11 20:04:50 -0500

    TP-54 Styles Sponsored Series masonry tile.

--- src/components/SponsoredSeries/SponsoredSeriesMasonryTile.js
+++ src/components/SponsoredSeries/SponsoredSeriesMasonryTile.js
@@ -1,14 +1,16 @@
 import React from "react";
 
+import styles from './SponsoredSeriesMasonryTile.module.scss';
+
 const SponsoredSeriesMasonryTile = (props) => {
     const series = {...props.data};
     const logo = (series.logo !== null) ? series.logo.url : "https://upload.wikimedia.org/wikipedia/en/d/da/Rlbug_100x100.png";
     const backgroundImage = (series.mainImage) ? series.mainImage.derivative.url : "https://upload.wikimedia.org/wikipedia/en/d/da/Rlbug_100x100.png";
     return (
 
-        <div style={{"backgroundImage": backgroundImage}}>
-            <h4>{series.name}</h4> {/* @TODO Need to make this a link when we have single series route. */}
-            <img src={logo} alt={""}/>
+        <div className={styles.tile} style={{"backgroundImage": backgroundImage}}>
+            <h4 className={styles.title}>{series.name}</h4> {/* @TODO Need to make this a link when we have single series route. */}
+            <img className={styles.logo} src={logo} alt={''}/>
         </div>
 
     );
