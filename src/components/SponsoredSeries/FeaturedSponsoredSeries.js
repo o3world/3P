088bac1e7458dd8e7bde9d7847ed27ceca6d93fc
Brent Danley <bdanley@3blmedia.com>
2018-12-11 19:34:47 -0500

    TP-74 Styles Featured Sponsored Series component.

--- src/components/SponsoredSeries/FeaturedSponsoredSeries.js
+++ src/components/SponsoredSeries/FeaturedSponsoredSeries.js
@@ -1,22 +1,22 @@
 import React from "react";
 
+import styles from './FeaturedSponsoredSeries.module.scss';
+
 const FeaturedSponsoredSeries = (props) => {
     let series = props.data.taxonomyTermQuery.entities[0];
     series =  (series !== undefined)? series: {"mainImage": {"derivative":{"url":""}}, "company": "", "logo":{"url":""}, "name": ""};
     return (
-        <div>
-            <img src={series.mainImage.derivative.url} alt={''}/>
-            <div>
-                <div>
-                    <p>{series.company}</p>
-                    <img src={series.logo.url} alt={series.company} />
+        <div className={styles.seriesWrapper}>
+            <img className={styles.heroImage} src={series.mainImage.derivative.url} alt={''}/>
+            <div className={styles.metaWrapper}>
+                <div className={styles.logoWrapper}>
+                  <p className={styles.logoTitle}>Featured Sponsor</p>
+                  <img className={styles.logo} src={series.logo.url} alt={series.company} />
                 </div>
-                <div>
-                    <div>
-                        <h3>{series.company}</h3>
-                        <h2>{series.name}</h2> {/* @TODO Need to make this a link when we have single series route. */}
-                    </div>
-                    <button>Explore Series</button>
+                <div className={styles.meta}>
+                    <span className={styles.company}>{series.company} Sponsored Series</span>
+                    <h3 className={styles.title}>{series.name}</h3> {/* @TODO Need to make this a link when we have single series route. */}
+                    <button className={styles.exploreButton}>Explore Series</button>
                 </div>
             </div>
         </div>
