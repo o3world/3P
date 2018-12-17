51cf7a842fa3a84814ae0a8d1bc7741844e33810
Brent Danley <bdanley@3blmedia.com>
2018-12-17 15:10:20 -0500

    Merges develop into editors feature.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import StoriesPage from './components/Stories/StoriesPage';
 import SponsoredSeriesAll from './components/SponsoredSeries/SponsoredSeriesAll';
-import SingleSeriesPage from "./components/SponsoredSeries/SingleSeriesPage";
 import Footer from "./components/Footer/Footer";
 import TheFeedPage from "./components/Feed/TheFeedPage";
 import EditorsPage from "./components/Editors/EditorsPage";
-import EditorDetailsPage from "./components/Editors/SingleEditorPage";
 import { ApolloClient } from "apollo-boost";
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
@@ -53,14 +51,9 @@ class App extends Component {
               <ApolloProvider client={client}>
                 <Header site_title={this.state.pageTitle}/>
                 <Route exact path={'/'} render={(props) => <Home {...props} newTitle={this.setPageTitle} />}/>
-                <Route path="/stories" component={StoriesPage}/>
-                <Route path={'/the-feed'} render={(props) => <TheFeedPage {...props} />}/>
-                <Route exact path="/editors" component={EditorsPage}/>
-                <Route path="/editor/:id" component={EditorDetailsPage}/>
-                <Route exact path={'/sponsored-series'} component={SponsoredSeriesAll}/>
                 <Route path={Routes.THEFEED} render={(props) => <TheFeedPage {...props} />}/>
-                <Route path={Routes.EDITORS} component={HomeEditors}/>
-                <Route exact path={Routes.SPONSOREDSERIES} component={SponsoredSeriesAll}/>
+                <Route path={Routes.EDITORS} component={EditorsPage}/>
+                <Route exact path={Routes.SPONSOREDSERIESALL} component={SponsoredSeriesAll}/>
                 <Route path={Routes.SINGLESPONSOREDSERIES} component={""}/>
                 <Route path={Routes.STORIES} component={StoriesPage}/>
                 <Route path={Routes.SINGLESTORY} component={""}/>
--- src/components/Common/RoutesConfig.js
+++ src/components/Common/RoutesConfig.js
@@ -1,13 +1,13 @@
 const Routes = {
-    "SPONSOREDSERIES": "/sponsored-series",
-    "SINGLESPONSOREDSERIES":"/sponsored-series/:id",
+    "SPONSOREDSERIESALL": "/sponsored-series",
+    "SINGLESPONSOREDSERIES": "/sponsored-series/:id",
     "EDITORS": "/editors",
-    "SINGLEEDITOR":"/editor/:id",
-    "THEFEED":"/the-feed",
-    "CONTACT":"/contact",
-    "STORIES":"/stories",
-    "SINGLESTORY":"/story/:id",
-    "SEARCH":"/search"
+    "SINGLEEDITOR": "/editor/:id",
+    "THEFEED": "/the-feed",
+    "CONTACT": "/contact",
+    "STORIES": "/stories",
+    "SINGLESTORY": "/story/:id",
+    "SEARCH": "/search"
 };
 
 export default Routes;
