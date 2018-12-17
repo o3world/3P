c4068a971f4cf1f22355c1bb11d9175c0afcd21b
santoshj-cuelogic <santosh.jagtap@cuelogic.co.in>
2018-12-17 19:22:37 +0530

    TP-69 added styles and links for social media details

--- src/components/Editors/EditorBio.js
+++ src/components/Editors/EditorBio.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import styles from './EditorCard.module.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const EditorBio = (props) => {
     const EditorDetails = props.data.userById;
     const EditorName = EditorDetails.fieldFirstName + " " + EditorDetails.fieldLastName;
@@ -8,9 +10,18 @@ const EditorBio = (props) => {
         <div>
           <div>
             <img src={EditorDetails.userPicture.url} />
-            <p>{EditorName} </p>
+            <p><h3 className={styles.name}>{EditorName}</h3></p>
             <p>{/* @TODO Add job title. */}</p>
-            <ul>{/* @TODO Add social icons and links. Use Font Awesome icons. */}</ul>
+              {/* @TODO Add social icons and links. Use Font Awesome icons. */}
+              {/* @TODO For now We are using static links for twitter and instagram but implementing the icons and styles */}
+            <ul>
+                <li>
+                    <p className={styles.title}><a href={'https://instagram.com/jhowell'} target={'_blank'}><FontAwesomeIcon className={styles.socialIcon} icon={["fab", "twitter"]} />@jhowell</a></p>
+                </li>
+                <li>
+                    <p className={styles.title}><a href={'https://twitter.com/jhowell'} target={'_blank'}><FontAwesomeIcon className={styles.socialIcon} icon={["fab", "instagram"]} />@jhowell</a></p>
+                </li>
+            </ul>
           </div>
           <div dangerouslySetInnerHTML={{__html: EditorDetails.fieldUserBio.processed}} />
           <div>
