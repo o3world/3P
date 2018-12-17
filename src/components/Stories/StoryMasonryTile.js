f6990451d97b1b2a3c352db81b8327d311f72e5c
santoshj-cuelogic <santosh.jagtap@cuelogic.co.in>
2018-12-17 22:45:05 +0530

    TP-65 Single editor page with Stories list masonary

--- src/components/Stories/StoryMasonryTile.js
+++ src/components/Stories/StoryMasonryTile.js
@@ -2,14 +2,15 @@ import React from "react";
 import Moment from 'react-moment';
 
 import styles from './StoryMasonryTile.module.scss';
+import ConvertSeoUrl from "../Common/createSeoUrl";
 
 const StoryMasonryTile = (props) => {
     const story = {...props.data};
     const backgroundImage =  story.fieldFeaturedImageTall.url;
-
+    const seoLink = ConvertSeoUrl(story.title);
     return (
         <div className={styles.tile} style={{backgroundImage: `url(`+backgroundImage+`)`}}>
-          <h2 className={styles.title}><a href={story.entityUrl}>{story.title}</a></h2>
+          <h2 className={styles.title}><a href="#" onClick={(e)=> props.onclick(e,story.nid,seoLink)}>{story.title}</a></h2>
           <p className={styles.category}>CATEGORY{/* @TODo Add category */}</p>
           <p className={styles.sponsored}>SPONSORED</p>
           <p className={styles.author}>Words by {story.entityOwner.name}</p>
