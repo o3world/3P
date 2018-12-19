c22fc19ca350d250d900a61e67cfeec45c1c049a
Brent Danley <bdanley@3blmedia.com>
2018-12-18 21:22:57 -0500

    TP-90 Styles single editor bio component.

--- src/components/Editors/EditorBio.js
+++ src/components/Editors/EditorBio.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import styles from "./EditorCard.module.scss";
+import styles from "./EditorBio.module.scss";
 
 const EditorBio = (props) => {
   const editor = props.data.userById;
@@ -9,34 +9,34 @@ const EditorBio = (props) => {
 
   let twitterIcon;
   if (editor.twitterUsername !== null) {
-    twitterIcon = <a href={'https://twitter.com/' + editor.twitterUsername} target={'_blank'}><FontAwesomeIcon className={styles.socialIcon} icon={["fab", "twitter"]} /></a>;
+    twitterIcon = <a href={'https://twitter.com/' + editor.twitterUsername} target={'_blank'} className={styles.twitterIcon}><FontAwesomeIcon className={styles.socialIcon} icon={["fab", "twitter"]} /> @{editor.twitterUsername}</a>;
   }
 
   let instagramIcon;
   if (editor.instagramUsername !== null) {
-    instagramIcon = <a href={'https://instagram.com/' + editor.instagramUsername} target={'_blank'}><FontAwesomeIcon className={styles.socialIcon} icon={["fab", "instagram"]} /></a>;
+    instagramIcon = <a href={'https://instagram.com/' + editor.instagramUsername} target={'_blank'} className={styles.instagramIcon}><FontAwesomeIcon className={styles.socialIcon} icon={["fab", "instagram"]} /> {editor.instagramUsername}</a>;
   }
 
   return(
-      <div>
-        <div>
-          <img src={editor.headshot.url} alt={fullName + ' headshot'}/>
-          <h1>{fullName} </h1>
-          <p>{editor.jobTitle}</p>
-          <ul>
-              <li>
-                {twitterIcon}
-              </li>
-              <li>
-                {instagramIcon}
-              </li>
-          </ul>
+      <section className={styles.wrapper}>
+        <img className={styles.headshot} src={editor.headshot.url} alt={fullName + ' headshot'}/>
+        <div className={styles.meta}>
+          <div className={styles.nameTitleWrapper}>
+            <h1 className={styles.name}>{fullName}</h1>
+            <p className={styles.jobTitle}>{editor.jobTitle}</p>
+          </div>
+          <div className={styles.socialWrapper}>
+            {twitterIcon}
+            {instagramIcon}
+          </div>
         </div>
-        <div dangerouslySetInnerHTML={{__html: editor.bio.processed}} />
-        <div>
+        <div className={styles.bio}>
+          <div dangerouslySetInnerHTML={{__html: editor.bio.processed}} />
+        </div>
+        <div className={styles.stories}>
           <p>Stories written by {fullName}</p>
         </div>
-      </div>
+      </section>
   );
 
 };
