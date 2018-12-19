9b7f1a5c3436fc6523aa1eb614b347fe933793f6
Brent Danley <bdanley@3blmedia.com>
2018-12-18 21:45:50 -0500

    TP-64 Adds Hero image to Editors page.

--- src/components/Editors/EditorsPage.js
+++ src/components/Editors/EditorsPage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import EditorCardsWrapper from './EditorCardsWrapper';
 
+import HeroImage from '../../assets/images/3pEditorsPageImage.jpg';
+
 class EditorsPage extends React.Component {
 
   render() {
     return (
       <section>
         <div>
-          <img src={''} alt={''} />
+          <img src={HeroImage} alt={'Editors Page Hero'} />
           <h1>Editors</h1>
         </div>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Istam voluptatem perpetuam quis potest praestare sapienti? Quo tandem modo? Et quidem, inquit, vehementer errat; Duo Reges: constructio interrete. Quamquam ab iis philosophiam et omnes ingenuas disciplinas habemus; Tu quidem reddes; Nec vero sum nescius esse utilitatem in historia, non modo voluptatem. Indicant pueri, in quibus ut in speculis natura cernitur. Expressa vero in iis aetatibus, quae iam confirmatae sunt. Ut placet, inquit, etsi enim illud erat aptius, aequum cuique concedere. His similes sunt omnes, qui virtuti student levantur vitiis, levantur erroribus, nisi forte censes Ti. Non quam nostram quidem, inquit Pomponius iocans;</p>
