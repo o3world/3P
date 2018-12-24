fe55b23368ccd394eadae9df74783ac6a7ca7e62
seacar <theseancarroll@gmail.com>
2018-12-24 14:11:45 -0600

    update size mapping and remove unneeded google script

--- src/components/Ads/BannerAd.js
+++ src/components/Ads/BannerAd.js
@@ -3,12 +3,12 @@ import { DFPSlotsProvider, AdSlot } from 'react-dfp';
 
 const BannerAd = () => {
   return (
-      <DFPSlotsProvider dfpNetworkId='21765524922'>
+      <DFPSlotsProvider dfpNetworkId='21765524922' sizeMapping={ [ {viewport: [1024, 768], sizes:[[728, 90]]}, {viewport: [900, 768], sizes:[[728, 90]] }] }>
         <div className="desktop-ads">
-          <AdSlot sizes={[[728,90], [300, 250]]} adUnit='Test_Banner_Ad' />
+          <AdSlot sizes={[[728,90]]} adUnit='Test_Banner_Ad' />
         </div>
         <div className="mobile-ads">
-          <AdSlot sizes={[[320,50], [300, 50]]} adUnit='Test_Banner_Ad' />
+          <AdSlot sizes={[[320,50]]} adUnit='Test_Banner_Ad' />
         </div>
         ...
       </DFPSlotsProvider>
