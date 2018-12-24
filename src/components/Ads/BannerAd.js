4596fc84b4493a1686a0b62aa62948fa21907bbe
seacar <theseancarroll@gmail.com>
2018-12-24 14:25:46 -0600

    update to collapse empty and autoload

--- src/components/Ads/BannerAd.js
+++ src/components/Ads/BannerAd.js
@@ -3,14 +3,13 @@ import { DFPSlotsProvider, AdSlot } from 'react-dfp';
 
 const BannerAd = () => {
   return (
-      <DFPSlotsProvider dfpNetworkId='21765524922' sizeMapping={ [ {viewport: [1024, 768], sizes:[[728, 90]]}, {viewport: [900, 768], sizes:[[728, 90]] }] }>
+      <DFPSlotsProvider dfpNetworkId='21765524922' autoLoad={ true } collapseEmptyDivs={ true } sizeMapping={ [ {viewport: [1024, 768], sizes:[[728, 90]]}, {viewport: [900, 768], sizes:[[728, 90]] }] }>
         <div className="desktop-ads">
           <AdSlot sizes={[[728,90]]} adUnit='Test_Banner_Ad' />
         </div>
         <div className="mobile-ads">
           <AdSlot sizes={[[320,50]]} adUnit='Test_Banner_Ad' />
         </div>
-        ...
       </DFPSlotsProvider>
   );
 };
