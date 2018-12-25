f6cda27e33ac3cba8175b4ede37c3155b3ea2df5
Brent Danley <bdanley@3blmedia.com>
2018-12-25 14:20:16 -0500

    TP-96 Adds change and submit handlers to newsletter signup form.

--- src/components/NewsletterSignup/NewsletterSignup.js
+++ src/components/NewsletterSignup/NewsletterSignup.js
@@ -3,20 +3,28 @@ import React from 'react';
 class NewsletterSignup extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {value: ''};
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleClick() {
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
     /* @TODO: code goes here. */
+    console.log('Email address ' + this.state.value + ' was submitted to Sendgrid');
+    event.preventDefault();
   }
 
   render() {
     return (
-        <div>
-          <input id={'subscriberEmail'}></input>
-          <button onClick={this.handleClick()}>Subscribe!</button>
-        </div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <button type="submit">Subscribe!</button>
+        </form>
     )
   }
 }
