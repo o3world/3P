2590f95cee391a466b6310ec548d9aebe686ce13
Prasana Alawekar <prasana.alawekar@cuelogic.co.in>
2018-12-27 14:45:39 +0530

    TP-97 async function added

--- src/components/NewsletterSignup/NewsLetterService.js
+++ src/components/NewsletterSignup/NewsLetterService.js
@@ -8,27 +8,35 @@ class NewsLetterService {
             method: "POST",
             body: JSON.stringify([{ "email": email }]),
             headers: {
-                "authorization": "Bearer "+API_KEY,
+                "authorization": "Bearer " + API_KEY,
                 "Content-Type": "application/json"
             },
         })
-        .then(response => {return response.json();})
-        .then(data => {
-            const newurl = "https://api.sendgrid.com/v3/contactdb/lists/6416658/recipients/" + data.persisted_recipients[0];
-            this.submitSecondRequest(newurl)
-        })
-        .catch(error => {console.log("error-------->", error);})
+            .then(response => { return response.json(); })
+            .then(async (data) => {
+                console.log('first---------data', data)
+                const newurl = "https://api.sendgrid.com/v3/contactdb/lists/6416658/recipients/" + data.persisted_recipients[0];
+                try {
+                    let status = await this.submitSecondRequest(newurl)
+                } catch (error) {
+                    console.log('error calling function')
+                }
+            })
+            .catch(error => { console.log("error-------->", error); })
     }
 
-    submitSecondRequest = (url) => {
-        fetch(url, {
-            method: "POST",
-            headers: {
-                "authorization": "Bearer "+API_KEY,
-            }
-        })
-        .then(response => { })
-        .catch(error => console.log('error2----->', error));
+    submitSecondRequest = async (url) => {
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "authorization": "Bearer " + API_KEY,
+                }
+            })
+            return response.status;
+        } catch (error) {
+            console.log('error2----->', error)
+        }
     }
 
 }
