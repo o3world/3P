cfb63b4f2f2f7d51c799b027e4e7539beafe9419
Prasana Alawekar <prasana.alawekar@cuelogic.co.in>
2018-12-27 16:43:27 +0530

    TP-99 valiadtion added to Newsletter Subsciption

--- src/components/NewsletterSignup/NewsletterSignup.js
+++ src/components/NewsletterSignup/NewsletterSignup.js
@@ -4,30 +4,45 @@ import NewsLetterService from './NewsLetterService';
 class NewsletterSignup extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       value: '',
-      successMessage : false
-     };
+      successMessage: false,
+      errorMessage: ""
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  valiadteEmail = (email) => {
+    var re = /^([a-z0-9_\-\.]+)@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/;
+    return re.test(email)
+  }
+
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ value: event.target.value, errorMessage: "",successMessage:false });
   }
 
   async handleSubmit(event) {
     /* @TODO: code goes here. */
     event.preventDefault();
-    console.log('Email address ' + this.state.value + ' was submitted to Sendgrid');
-    let status = await NewsLetterService.submitEmail(this.state.value);
-    if(status === 201) {
+    let result = this.valiadteEmail(this.state.value);
+    if (result) {
+      let status = await NewsLetterService.submitEmail(this.state.value);
+      if(status === 201) {
+        this.setState({
+          ...this.state,
+          successMessage: true
+        })
+      }
+    } else {
       this.setState({
         ...this.state,
-        successMessage: true
+        errorMessage: "Plese enter valid email"
       })
     }
+
+
   }
 
   render() {
@@ -36,7 +51,8 @@ class NewsletterSignup extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <input type="text" value={this.state.value} onChange={this.handleChange} />
           <button type="submit">Subscribe!</button>
-         {this.state.successMessage ? <h3>Thank you for subscription!!!</h3> : null } 
+          {this.state.successMessage ? <h3>Thank you for subscription!!!</h3> : null}
+          <h3>{this.state.errorMessage}</h3>
         </form>
       </React.Fragment>
     )
