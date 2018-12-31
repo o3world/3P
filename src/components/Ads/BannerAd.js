79df2116adf080e0d7b786aed3252de94ada9498
seacar <theseancarroll@gmail.com>
2018-12-31 11:10:01 -0600

    update responsive viewport size

--- src/components/Ads/BannerAd.js
+++ src/components/Ads/BannerAd.js
@@ -4,7 +4,7 @@ import { DFPSlotsProvider, AdSlot } from 'react-dfp';
 //creates basic banner ad
 const BannerAd = () => {
   return (
-      <DFPSlotsProvider dfpNetworkId='21765524922' autoLoad={ true } collapseEmptyDivs={ true } sizeMapping={ [ {viewport: [1024, 768], sizes:[[728, 90]]}, {viewport: [900, 768], sizes:[[728, 90]] }] }>
+      <DFPSlotsProvider dfpNetworkId='21765524922' autoLoad={ true } collapseEmptyDivs={ true } sizeMapping={ [ {viewport: [1024, 768], sizes:[[728, 90]]}, {viewport: [414, 896], sizes:[[320, 50]] }] }>
         <div className="desktop-ads">
           <AdSlot sizes={[[728,90], [320,50]]} adUnit='Test_Banner_Ad' />
         </div>
