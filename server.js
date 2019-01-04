5d0bf53b160d7d233bcd30874f350bf44fa396e1
seacar <theseancarroll@gmail.com>
2019-01-04 10:43:25 -0600

    remove junk from server

--- server.js
+++ server.js
@@ -7,16 +7,10 @@ const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 //production mode
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'build/index.html'));
-  })
-}
-//build mode
+app.use(express.static(path.join(__dirname, 'build')));
+//
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'public/index.html'));
+  res.sendfile(path.join(__dirname = 'build/index.html'));
 })
 
 //start server
