e162a548fdfc2d8ccbbfa5e0636d9936ccace942
Brent Danley <bdanley@3blmedia.com>
2019-01-07 17:43:56 -0500

    TP-89 Removes explicit return. Sets mobile viewport to [0,0]. Adds Sass module.

--- src/components/Ads/Ad.js
+++ src/components/Ads/Ad.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { DFPSlotsProvider, AdSlot } from 'react-dfp';
 
-//creates basic banner ad
-const Ad = (props) => {
-  return (
-      <DFPSlotsProvider dfpNetworkId='21765524922' autoLoad={ true } collapseEmptyDivs={ true } sizeMapping={ [ {viewport: [1024, 768], sizes:[[728, 90]]}, {viewport: [414, 896], sizes:[[320, 50]] }] }>
-        <div className="desktop-ads">
-          <AdSlot sizes={[[728,90], [320,50]]} adUnit={props.adUnit}/>
-        </div>
-      </DFPSlotsProvider>
-  );
-};
+import styles from './Ad.module.scss';
+
+const Ad = (props) =>
+  <DFPSlotsProvider dfpNetworkId='21765524922' autoLoad={ true } collapseEmptyDivs={ true } sizeMapping={ [ {viewport: [1024, 768], sizes:[[728, 90]]}, {viewport: [0,0], sizes:[[320, 50]] }] }>
+    <div className={styles.ad}>
+      <AdSlot sizes={[[728,90], [320,50]]} adUnit={props.adUnit}/>
+    </div>
+  </DFPSlotsProvider>;
 
 export default Ad;
