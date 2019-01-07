a9b8c8144d04c54139defb20c81e2e22a6402f66
seacar <theseancarroll@gmail.com>
2019-01-07 10:14:07 -0600

    fix npm error and ad warning

--- src/components/Ads/Ad.js
+++ src/components/Ads/Ad.js
@@ -6,7 +6,7 @@ const Ad = (props) => {
   return (
       <DFPSlotsProvider dfpNetworkId='21765524922' autoLoad={ true } collapseEmptyDivs={ true } sizeMapping={ [ {viewport: [1024, 768], sizes:[[728, 90]]}, {viewport: [414, 896], sizes:[[320, 50]] }] }>
         <div className="desktop-ads">
-          <AdSlot sizes={[[728,90], [320,50]]} adUnit={props.adUnit}/> //ad unit must match google ad manager code name
+          <AdSlot sizes={[[728,90], [320,50]]} adUnit={props.adUnit}/> {/*ad unit must match google ad manager code name*/}
         </div>
       </DFPSlotsProvider>
   );
