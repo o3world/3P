99c4e814c8e9d58ddd6d62381d6ed2773747db3b
seacar <theseancarroll@gmail.com>
2019-01-07 10:08:02 -0600

    comment ads

--- src/components/Ads/Ad.js
+++ src/components/Ads/Ad.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { DFPSlotsProvider, AdSlot } from 'react-dfp';
+import { DFPSlotsProvider, AdSlot } from 'react-dfp'; //https://github.com/jaanauati/react-dfp/
 
 //creates basic banner ad
 const Ad = (props) => {
   return (
       <DFPSlotsProvider dfpNetworkId='21765524922' autoLoad={ true } collapseEmptyDivs={ true } sizeMapping={ [ {viewport: [1024, 768], sizes:[[728, 90]]}, {viewport: [414, 896], sizes:[[320, 50]] }] }>
         <div className="desktop-ads">
-          <AdSlot sizes={[[728,90], [320,50]]} adUnit={props.adUnit}/>
+          <AdSlot sizes={[[728,90], [320,50]]} adUnit={props.adUnit}/> //ad unit must match google ad manager code name
         </div>
       </DFPSlotsProvider>
   );
