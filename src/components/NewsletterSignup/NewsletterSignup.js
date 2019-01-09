e63caf804502f107222f6cf343732d6bb8f67af8
Brent Danley <bdanley@3blmedia.com>
2019-01-08 20:27:58 -0500

    TP-97 Removes extraneous state spread syntax lines. Combines newsletter methods into single class.

--- src/components/NewsletterSignup/NewsletterSignup.js
+++ src/components/NewsletterSignup/NewsletterSignup.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import NewsLetterService from './NewsLetterService';
+import { SENDGRID } from '../Common/Constants';
 
 class NewsletterSignup extends React.Component {
-  constructor(props) {
+  constructor( props ) {
     super(props);
     this.state = {
       value: '',
@@ -11,13 +11,56 @@ class NewsletterSignup extends React.Component {
       disabled: false
     };
 
+    this.addEmailToList = this.addEmailToList.bind(this);
+    this.addEmailToSendGrid = this.addEmailToSendGrid.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  valiadteEmail = (email) => {
+  ErrorMessage = "Something went wrong please try again";
+  SuccessMessage = "Thank You!!!";
+
+  validateEmail = (email) => {
     const re = /^([a-z0-9_\-.]+)@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/;
     return re.test(email)
+  };
+
+  addEmailToSendGrid = async (email) => {
+    try {
+      const response = await fetch(SENDGRID.API, {
+        method: "POST",
+        body: JSON.stringify([{ "email": email }]),
+        headers: {
+          "authorization": "Bearer " + SENDGRID.API_KEY,
+          "Content-Type": "application/json"
+        }
+      });
+
+      let data = await response.json();
+      try {
+        let status = await this.addEmailToList(SENDGRID.LIST_API + data.persisted_recipients[0]);
+        if (status === 201) {
+          return this.SuccessMessage;
+        } else {
+          return this.ErrorMessage
+        }
+      } catch (error) { return this.ErrorMessage }
+    } catch (error) { return this.ErrorMessage }
+  };
+
+  addEmailToList = async (url) => {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "authorization": "Bearer " + SENDGRID.API_KEY,
+        }
+      });
+      return response.status;
+    } catch (error) {
+      return this.ErrorMessage
+    }
   };
 
   handleChange(event) {
@@ -27,16 +70,14 @@ class NewsletterSignup extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
 
-    let result = this.valiadteEmail(this.state.value);
+    let result = this.validateEmail(this.state.value);
     
     if(result) {
       this.setState({
-        ...this.state,
         disabled: true
       }, async () => {
-        let message = await NewsLetterService.addEmailToSendGrid(this.state.value);
+        let message = await this.addEmailToSendGrid(this.state.value);
         this.setState({
-          ...this.state,
           successMessage: message,
           value: "",
           disabled: false
@@ -44,24 +85,19 @@ class NewsletterSignup extends React.Component {
       })
     } else {
       this.setState({
-        ...this.state,
         validationMessage:"Please enter valid email"
       })
     }
-
-    
   }
 
   render() {
     return (
-      <React.Fragment>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-          <button type="submit" disabled={this.state.disabled}>Subscribe!</button>
-          <h3>{this.state.successMessage}</h3>
-          <h3>{this.state.validationMessage}</h3>
-        </form>
-      </React.Fragment>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <button type="submit" disabled={this.state.disabled}>Subscribe!</button>
+        <h3>{this.state.successMessage}</h3>
+        <h3>{this.state.validationMessage}</h3>
+      </form>
     )
   }
 }
