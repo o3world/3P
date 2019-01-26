ee437e094d8fdd97d7f4812493ab7f23e180056e
Brent Danley <bdanley@3blmedia.com>
2019-01-26 00:31:14 -0500

    Updates BTS block on homepage with current newsletter title and date.

--- src/components/BTS/HomeBTS.js
+++ src/components/BTS/HomeBTS.js
@@ -12,9 +12,9 @@ const HomeBTS = () => {
           <div className={styles.contentWrapper}>
             <div className={styles.dateWrapper}>
             <h4>Weekly Newsletter</h4>
-              <time className={styles.date}>Jan 16</time>
+              <time className={styles.date}>Jan 23</time>
             </div>
-            <h3 className={styles.title}>For Consumers, CSR Is Now More Important Than Price</h3>
+            <h3 className={styles.title}>The First Climate-Change Bankruptcy Is a Wake Up Call</h3>
             <p className={styles.author}>Words by John Howell</p>
           </div>
           <a href={'http://www.3blforum.com/brands-taking-stands-newsletter/newsletter-sign-up'} className={styles.button} target={'_blank'}>Subscribe</a>
