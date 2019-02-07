062e9a859445a30ef2a124d89dab29121338a899
Brent Danley <bdanley@3blmedia.com>
2019-02-07 14:15:41 -0500

    TP-143 Cleans up spacing on 404 component.

--- src/components/404/404.js
+++ src/components/404/404.js
@@ -5,14 +5,14 @@ import StoriesMasonryWrapper from '../Stories/StoriesMasonryWrapper'
 import Footer from '../Footer/Footer'
 
 const the404 = () =>
-    <div>
-      <Header/>
-      <section>
-        <h2>Oops, looks like we couldn't find your page (404).</h2>
-        <p>While you're here, read one of these important stories.</p>
-      </section>
-      <StoriesMasonryWrapper/>
-      <Footer/>
-    </div>;
+  <div>
+    <Header/>
+    <section>
+      <h2>Oops, looks like we couldn't find your page (404).</h2>
+      <p>While you're here, read one of these important stories.</p>
+    </section>
+    <StoriesMasonryWrapper/>
+    <Footer/>
+  </div>;
 
 export default the404;
