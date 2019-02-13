892994cd01cbf66859c854b82277846edf3fa213
William Wolfrath <william.wolfrath@gmail.com>
2019-02-13 15:04:19 -0500

    Updates BTS card on home page per John H.

--- src/components/BTS/HomeBTS.js
+++ src/components/BTS/HomeBTS.js
@@ -9,15 +9,15 @@ const HomeBTS = () => {
   };
     return (
         <div className={styles.wrapper} style={homeBTSStyle}>
-          <div className={styles.contentWrapper}>
-            <div className={styles.dateWrapper}>
-            <h4>Weekly Newsletter</h4>
-              <time className={styles.date}>Feb 6</time>
+            <div className={styles.contentWrapper}>
+                <div className={styles.dateWrapper}>
+                    <h4>Weekly Newsletter</h4>
+                    <time className={styles.date}>Feb 13</time>
+                </div>
+                <h3 className={styles.title}>Purpose + Corporate Activism = A Winning Formula.</h3>
+                <p className={styles.author}>Words by John Howell</p>
             </div>
-            <h3 className={styles.title}>Purpose-Led Brands Take the Lead in Business</h3>
-            <p className={styles.author}>Words by John Howell</p>
-          </div>
-          <a href={'http://www.3blforum.com/brands-taking-stands-newsletter/newsletter-sign-up'} className={styles.button} target={'_blank'}>Subscribe</a>
+            <a href={'http://www.3blforum.com/brands-taking-stands-newsletter/newsletter-sign-up'} className={styles.button} target={'_blank'}>Subscribe</a>
         </div>
     )
 };
