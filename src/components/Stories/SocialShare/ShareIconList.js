f925c8eb2bca3f0b61de526113e88615e431e4c2
Brent Danley <bdanley@3blmedia.com>
2019-03-31 00:16:40 -0400

    TP-157 Removed import of Component in shareiconlist

--- src/components/Stories/SocialShare/ShareIconList.js
+++ src/components/Stories/SocialShare/ShareIconList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './ShareIconList.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
