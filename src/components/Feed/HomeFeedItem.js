8b236c0f50d6a524e9dd0e155eff671f2d65ecaf
Brent Danley <bdanley@3blmedia.com>
2019-06-03 11:06:14 -0400

    TP-189 Adds a no-image variant to the homepage feed cards

--- src/components/Feed/HomeFeedItem.js
+++ src/components/Feed/HomeFeedItem.js
@@ -8,7 +8,7 @@ const HomeFeedItem = (props) => {
   const timeFromNowDuring = 2 * 24 * 60 * 60 * 60 * 1000; // 2 days of milliseconds
 
   return (
-      <div className={styles.item} style={{backgroundImage: `url(${props.image})`}}>
+      <div className={props.image ? styles.item : styles.itemNoImage} style={{backgroundImage: props.image ? `url(${props.image})` : "none"}}>
           <div className={styles.metaContainer}>
             <Moment className={styles.date} format={'MMM DD'} fromNowDuring={timeFromNowDuring}>{props.date}</Moment>
               <h3 className={styles.title}><a target='_blank' rel="noopener noreferrer" href={props.link}>{props.title}</a></h3>
