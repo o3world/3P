e070e5531a93c0e6de7b90d7fe582d1ee748f0bb
Brent Danley <bdanley@3blmedia.com>
2019-09-30 15:03:24 -0400

    TP-241 Copy changes on Editors Page

--- src/components/Editors/Contributors.js
+++ src/components/Editors/Contributors.js
@@ -20,7 +20,7 @@ import styles from './Contributors.module.scss'
             <div className={styles.wrapper}>
                 <h2 className={styles.heading}>Contributors</h2>
               <hr className={styles.rule}/>
-                <div className={styles.contribCopy}>Triple Pundit’s staff of writers a wide array of professional and personal experiences to bear on original reporting. Contributors include experienced journalists, executives and consultants who have worked with Fortune 500 companies, and activists supporting social innovation or environmental stewardship through work at nonprofits. Located around the world, our writers bring in-depth, global perspectives to the issues that matter to business today.</div>
+                <div className={styles.contribCopy}>TriplePundit’s staff of writers a wide array of professional and personal experiences to bear on original reporting. Contributors include experienced journalists, executives and consultants who have worked with Fortune 500 companies, and activists supporting social innovation or environmental stewardship through work at nonprofits. Located around the world, our writers bring in-depth, global perspectives to the issues that matter to business today.</div>
               <ul className={styles.contribList}>
               {
                 contributors.map((contributor, index) =>
--- src/components/Editors/EditorsPage.js
+++ src/components/Editors/EditorsPage.js
@@ -17,8 +17,8 @@ class EditorsPage extends React.Component {
           <h1 className={styles.pageTitle}>Editors</h1>
         </div>
         <div className={styles.editorsDescription}>
-          <p>For over a decade, TriplePundit has reported on the intersection of people, planet and profit, believing business can be a force for good. We make the business case for corporate responsibility and sustainability, and the private sector's leadership to secure social justice and fair economic opportunities for all. The bottom line: Triple Pundit reports on the business of doing better.</p>
-          <p>Triple Pundit provides conceptual context for a readership of CSR and sustainability professionals, C-suite executives, the corporate workforce, journalists, academics, activists, NGOs, and concerned citizens. A staff of expert writers contributes analysis, insights, and opinion on issues at the top of today&#39;s business agenda. Those issues include climate change, diversity and inclusion, socially responsible investment, employee engagement, corporate governance, and responsible supply chain management.</p></div>
+          <p>For over a decade, TriplePundit has reported on the intersection of people, planet and profit, believing business can be a force for good. We make the business case for corporate responsibility and sustainability, and the private sector's leadership to secure social justice and fair economic opportunities for all. The bottom line: TriplePundit reports on the business of doing better.</p>
+          <p>TriplePundit provides conceptual context for a readership of CSR and sustainability professionals, C-suite executives, the corporate workforce, journalists, academics, activists, NGOs, and concerned citizens. A staff of expert writers contributes analysis, insights, and opinion on issues at the top of today&#39;s business agenda. Those issues include climate change, diversity and inclusion, socially responsible investment, employee engagement, corporate governance, and responsible supply chain management.</p></div>
         <EditorCardsWrapper/>
         <Contributors/>
       </PageTemplate>
